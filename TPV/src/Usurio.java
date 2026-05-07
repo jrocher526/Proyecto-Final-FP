039d804aa8870e12b9a6cf32689a5b68c385fc14
dsanvaz2103 <151721658+dsanvaz2103@users.noreply.github.com>
2026-05-07 12:50:45 +0200

    Refactor Usurio class to abstract Usuario class

--- TPV/src/Usurio.java
+++ TPV/src/Usurio.java
@@ -1,2 +1,18 @@
-public class Usurio {
+public abstract class Usuario {
+    protected int id;
+    protected String nombre;
+    protected String password;
+
+    public boolean login(String pass) {
+        return this.password.equals(pass);
+    }
+}
+
+// Camarero.java
+package com.tpv.usuarios;
+import com.tpv.modelos.Mesa;
+
+public class Camarero extends Usuario {
+    public void seleccionarMesa(Mesa m) { /* Lógica */ }
+    public void crearTicket(Mesa m) { /* Lógica */ }
 }
