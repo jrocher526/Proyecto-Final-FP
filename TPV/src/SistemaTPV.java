bb1fa2d055751e6e797a9c3696ad920fc97cf6b0
dsanvaz2103 <151721658+dsanvaz2103@users.noreply.github.com>
2026-05-07 12:52:19 +0200

    Update SistemaTPV.java

--- TPV/src/SistemaTPV.java
+++ TPV/src/SistemaTPV.java
@@ -1,2 +1,15 @@
 public class SistemaTPV {
+    private Date fechaSesion;
+    private float totalCaja;
+    private int numeroTickets;
+
+    public void iniciarSesion() {
+        this.fechaSesion = new Date();
+        this.totalCaja = 0;
+    }
+
+    public void registrarTicket(Ticket t) {
+        this.totalCaja += t.getTotal();
+        this.numeroTickets++;
+    }
 }
