c74338af2759add119d815d8dd7c5d55431b46ba
pjimpin1207 <pjimpin1207@g.educaand.es>
2026-05-12 14:48:35 +0200

    arreglar fallos en las clases

--- TPV/src/Camarero.java
+++ TPV/src/Camarero.java
@@ -1,4 +1,8 @@
 public class Camarero extends Usuario {
-    public void seleccionarMesa(Mesa m) {  }
-    public void crearTicket(Mesa m) { }
+    public void seleccionarMesa(Mesa m) {
+
+    }
+    public void crearTicket(Mesa m) {
+        
+    }
 }
--- TPV/src/SistemaTPV.java
+++ TPV/src/SistemaTPV.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 public class SistemaTPV {
     private Date fechaSesion;
     private float totalCaja;
